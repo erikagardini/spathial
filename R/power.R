1c8dbbeef8f2b040099de14f3235ab0b644d4320
Erika <erika.gardini3@studio.unibo.it>
2019-06-13 09:11:46 +0200

    aa

---
+++ R/power.R
@@ -0,0 +1,9 @@
+#' Square a number
+#'
+#' Takes in any numeric value and squares it.
+#' @param x A numeric value to be squared
+#' @return The square of the input
+#' @export
+square <- function(x){
+  return(x^2)
+}
