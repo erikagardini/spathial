625aff316f6b39bd7a513e954c5a602b9aeb8c55
federicogiorgi <federico.giorgi@gmail.com>
2019-06-16 01:06:02 +0200

    Committing from rstudio is hard

--- inst/scripts/howto.R
+++ inst/scripts/howto.R
@@ -2,13 +2,23 @@
 # git config --global user.name "federicogiorgi"
 # git config --global user.email "federico.giorgi@gmail.com"
 
+### Switching remote URLs from SSH to HTTPS
+# # git remote -v
+# >origin  https://github.com/erikagardini/spathial (fetch)
+# >origin  https://github.com/erikagardini/spathial (push)
+git remote set-url origin https://github.com/erikagardini/spathial.git
 
-if (!require(git2r)) {
-  install.packages("git2r", repos="http://cran.r-project.org/", quiet=TRUE)
-}
-library(git2r)
-repo<-repository("")
-push(repo, credentials=cred_user_pass("federicogiorgi",pass))
+
+### Switching remote URLS from HTTPS to SSH
+git remote set-url origin git@github.com:erikagardini/spathial.git
+
+
+# if (!require(git2r)) {
+#   install.packages("git2r", repos="http://cran.r-project.org/", quiet=TRUE)
+# }
+# library(git2r)
+# repo<-repository(".")
+# push(repo, credentials=cred_user_pass("federicogiorgi",pass))
 
 
 # Building manuals and namespace using roxygen2
