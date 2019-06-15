da2b5a0aa0805f8699d9c56b12073925486091f3
federicogiorgi <federico.giorgi@gmail.com>
2019-06-16 00:55:32 +0200

    How to prevent rstudio from asking to authenticate every time

--- inst/scripts/howto.R
+++ inst/scripts/howto.R
@@ -1,7 +1,14 @@
 ### Git tricks
 # git config --global user.name "federicogiorgi"
-#
+# git config --global user.email "federico.giorgi@gmail.com"
 
+
+if (!require(git2r)) {
+  install.packages("git2r", repos="http://cran.r-project.org/", quiet=TRUE)
+}
+library(git2r)
+repo<-repository("")
+push(repo, credentials=cred_user_pass("federicogiorgi",pass))
 
 
 # Building manuals and namespace using roxygen2
